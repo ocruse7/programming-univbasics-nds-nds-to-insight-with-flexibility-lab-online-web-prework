0334ebb65f82ec3efb3d376f565aaac6ae488fec
Owen Cruse <ocruse7@gmail.com>
2019-10-25 18:49:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -103,7 +103,14 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
+  index = 0
+  movieList = [[]]
   
+  while index < source.length do
+    movieList[index] = movies_with_director_key(source[index][:name], source[index][:movies])
+    index += 1
+  end
+  movieList
   
 end
 
